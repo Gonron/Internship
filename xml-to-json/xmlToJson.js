ea69eac600fe91c3e0fe7b69fc539b431c8849b7
Gonron <lundsgaard1997@hotmail.dk>
2019-08-27 12:52:42 +0200

    working on getting data from xml

--- xml-to-json/xmlToJson.js
+++ xml-to-json/xmlToJson.js
@@ -1,19 +1,37 @@
-const convert = require('xml-js')
-const fs = require('fs')
 const DOMParser = require('xmldom').DOMParser
-const xml = require('./xml')
+const xml = require('./xml2')
 
-// let res = convert.xml2json(xml2, { compact: false, spaces: 4 })
 let doc = new DOMParser().parseFromString(xml, 'text/xml')
 
-for (let i = 0; i < 5; i++) {
-	console.log(doc.getElementsByTagName('e:Revenue')[i].textContent)
-}
-// console.log(doc.getElementsByTagName('contextRef="c65"'))
+/* TODO:
+ *	- find a way to identify if an xml file uses fsa or e
+ *	- make it so it takes an url instead of a file
+ *
+ */
 
-// fs.writeFile('json.json', res, function(err) {
-// 	if (err) {
-// 		return console.log(err)
-// 	}
-// 	console.log('File has been written')
-// })
+// Handler for different revenue names
+let revTag = ''
+if (doc.getElementsByTagName('e:Revenue').length > 0) {
+	console.log('rev')
+	revTag = 'e:Revenue'
+} else {
+	console.log('gpl')
+	revTag = 'e:GrossProfitLoss'
+}
+
+// To get data with a <period> tag
+for (let i = 0; i < doc.getElementsByTagName(revTag).length; i++) {
+	let revenue = doc.getElementsByTagName(revTag)[i].textContent
+	let contexRef = doc.getElementsByTagName(revTag)[i].attributes[0].nodeValue
+	let start = doc.getElementById(contexRef).childNodes[3].childNodes[1].childNodes[0].nodeValue
+	let end = doc.getElementById(contexRef).childNodes[3].childNodes[3].childNodes[0].nodeValue
+	console.log('rev:', revenue, '- ref:', contexRef, '- sDate:', start, '- eDate', end)
+}
+
+// To get data with a <instant> tag
+for (let i = 0; i < doc.getElementsByTagName('e:Assets').length; i++) {
+	let data = doc.getElementsByTagName('e:Assets')[i].textContent
+	let ref = doc.getElementsByTagName('e:Assets')[i].attributes[0].nodeValue
+	let period = doc.getElementById(ref).childNodes[3].childNodes[1].childNodes[0].nodeValue
+	console.log(ref, period, data)
+}
